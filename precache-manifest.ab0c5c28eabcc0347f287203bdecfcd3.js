a50c35f1c87e74196001859437df5e7a0c52436d
David Barbosa <davidsilva.sp@outlook.com>
2019-07-21 01:37:42 -0300

    Updates

---
+++ precache-manifest.ab0c5c28eabcc0347f287203bdecfcd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "931168a770982c5b32bb17e440c9dc96",
+    "url": "/davidsilvasp/my-reads/static/media/more-vertical.931168a7.svg"
+  },
+  {
+    "revision": "d255e67839cd69df2bb7",
+    "url": "/davidsilvasp/my-reads/static/js/runtime~main.d255e678.js"
+  },
+  {
+    "revision": "7ca2faba38d7f769048c",
+    "url": "/davidsilvasp/my-reads/static/js/main.7ca2faba.chunk.js"
+  },
+  {
+    "revision": "00d103f0c6db0464a4e9",
+    "url": "/davidsilvasp/my-reads/static/js/2.00d103f0.chunk.js"
+  },
+  {
+    "revision": "7ca2faba38d7f769048c",
+    "url": "/davidsilvasp/my-reads/static/css/main.eabd5bfc.chunk.css"
+  },
+  {
+    "revision": "aeeb65414562f52fba2c9e78b01eca2b",
+    "url": "/davidsilvasp/my-reads/index.html"
+  }
+];
